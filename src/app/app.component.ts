4c8b9a96eb0269548c153e5b1f495a4e01c66283
lzjslqq <chief.king@qq.com>
2017-07-27 13:47:01 +0800

    英雄编辑器开始

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} 信息!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>
+  <input  placeholder="输入英雄名称" [(ngModel)]='hero.name' />
+  {{hero.name}}
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '英雄之旅';
+  hero: Hero = { id: 1, name: '风暴' };
 }
